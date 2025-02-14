8c128567f7e4f654ad83a86abbaae6cf5fa34c3c
Álvaro V. <165289321+fgbbd@users.noreply.github.com>
2025-02-14 22:57:54 +0100

    Remove emercency & add footer to games

--- frontend/emercency.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.ctrlKey && event.key.toLowerCase() === "a") {
-        event.preventDefault();
-        window.open("https://es.wikipedia.org/wiki/Especial:Aleatoria", "_blank");
-    }
-});
