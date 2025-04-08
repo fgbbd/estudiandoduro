97b1ae5956dc3244efe3fe712d17a25db01a620f
Álvaro V. <alvarobotdc@gmail.com>
2025-04-08 15:42:33 +0200

    Add Super Star Car

--- content/download.py
+++ content/download.py
@@ -164,9 +164,9 @@ def download_images(game_url, dir_name):
             print(f"❌ Error al descargar {url}: {e}")
 
 if __name__ == "__main__":
-    input_url = 'https://bitlifeonline.github.io/level-devil/'
-    dir_name = 'level_devil'
+    input_url = 'https://htmlxm.github.io/h3/super-star-car/'
+    dir_name = 'super_star_car'
     download_images(input_url, dir_name)
     # download_assets(input_url, os.path.join('game', dir_name))
     # download_file('https://watchdocumentaries.com/wp-content/uploads/games/granny-2/Build/Granny%202.loader.js', 'granny2', 'https://watchdocumentaries.com/wp-content/uploads/games/granny-2/')
-    # build_data('game.json', input_url, f'game/{dir_name}/')
+    # build_data('Build.json', input_url, f'game/{dir_name}')
