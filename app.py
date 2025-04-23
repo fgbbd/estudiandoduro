8faf7c4a20579f879f2d74ab565788fdf3440f44
fgbbd <alvarobotdc@gmail.com>
2025-04-23 12:48:12 +0000

    feat: add route

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import flask, request
+from github import Github
+
+app = __name__
+
+@app.route('/pr', methods=['POST'])
+def pr():
+    """Json: {"frase": frase}"""
+    data = request.get_json()
+    frase = data['frase']
+
+
+
+
