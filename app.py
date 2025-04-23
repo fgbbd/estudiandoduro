d43db48bcbebced6bb19b4f5740b415688156688
fgbbd <alvarobotdc@gmail.com>
2025-04-23 13:09:01 +0000

    feat: add function to create pr

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
+import os
+import json
+
 from flask import flask, request
 from github import Github
+from dotenv import load_dotenv
+
+TOKEN = os.getenv('GITHUB_TOKEN')
+g = Github(TOKEN)
+repo_name = "fgbbd/estudiandoduro"
+repo = g.get_repo(repo_name)
 
 app = __name__
 
-@app.route('/pr', methods=['POST'])
+@app.route('/', methods=['GET'])
 def pr():
-    """Json: {"frase": frase}"""
-    data = request.get_json()
-    frase = data['frase']
+    frase = request.args.get('frase')
+    persona = request.args.get('persona')
 
-    
+    file = repo.get_contents(f"public/data/{persona}")
 
+    frases_anteriores = json.load(file)
+    frases_anteriores.append(frase)
+
+    create_pr(persona, frases_anteriores, frase)
+
+def create_pr(persona, frases, frase):
+    base = repo.get_branch('main')
+    repo.create_git_ref(ref="refs/heads/development", sha=base.commit.sha)
+    pr_title = f"Nueva frase sugerida de {persona}"
+    pr_description = f'## Nueva frase sugerida\n**Frase de {persona}: `{frase}`**'
+    repo.create_pull(title=pr_title, body=pr_description, head='development', base='main')
 
 
