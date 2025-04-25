6b2d48befa79d0562957643f95f4d01818a04dc5
Álvaro V. <alvarobotdc@gmail.com>
2025-04-25 23:46:25 +0200

    feat: add tailwindcss and custom fonts
    - Added Tailwind CSS for styling
    - Added custom fonts (Bungee, Montserrat)
    - Updated header and layout styling
    - Improved global styles with theme
    - Updated package.json and astro.config.ts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'background': '#1e1e2e',
+          'text': '#cdd6f4',
+        },
+        fontFamily: {
+            Bungee: ['Bungee', 'sans-serif'],
+          }
+      }
+    }
+  }
